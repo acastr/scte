69c01a381e3ed2c931eb028121f3ecdf96c47d9e
Anthony Castreuil <anthony@castreuil.be>
2026-06-25 20:53:02 +0200

    Init function now derives __version__ from installed package metadata

--- scte/__init__.py
+++ scte/__init__.py
@@ -1,2 +1,9 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("scte")
+except PackageNotFoundError:  # not installed (e.g. running from a source tree)
+    __version__ = "0.0.0+unknown"
+
 from scte import Scte35
 from scte import Scte104
